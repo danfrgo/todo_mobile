daf91724d143924e0718210a332c017c9a2b59b3
danfrgo <danielfr_goncalves@hotmail.com>
2021-01-15 12:43:01 +0000

    Inicio Projeto

---
+++ src/views/Home/styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF',
+        alignItems: 'center', // fazz o alinhamento vertical do ecra
+        justifyContent: 'center' // fazz o alinhamento horizontal do ecra
+    }
+});
+
+export default styles;
