7c10c20da0fea9dfeb6edf53ea2a7f47016354c4
danfrgo <danielfr_goncalves@hotmail.com>
2021-01-15 23:47:31 +0000

    Desenvolvimento dos filtros

--- src/views/Home/styles.js
+++ src/views/Home/styles.js
@@ -2,11 +2,47 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#FFF',
-        alignItems: 'center', // fazz o alinhamento vertical do ecra
-        justifyContent: 'flex-start' // fazz o alinhamento horizontal do ecra
+      flex: 1,
+      backgroundColor: '#FFF',
+      alignItems: 'center', // faz o alinhamento vertical do ecra
+      justifyContent: 'flex-start' // fazz o alinhamento horizontal do ecra
+    },
+    filter: {
+      flexDirection: 'row',
+      width: '100%',
+      justifyContent: 'space-around',
+      height: 70,
+      alignItems: 'center'
+    },
+    filterTextActived: {
+      fontWeight: 'bold',
+      fontSize: 18,
+      color: '#EE6B26'
+    },
+    filterTextInative: {
+      color: '#20295f',
+      fontWeight: 'bold',
+      fontSize: 18,
+      opacity: 0.5
+    },
+    content: {
+      width: '100%',
+      marginTop: 30
+    },
+    title: {
+      width: '100%',
+      borderBottomWidth: 1,
+      borderColor: '#20295f',
+      alignItems: 'center'
+    },
+    titleText: {
+      color: '#20295f',
+      fontSize: 18,
+      position: 'relative',
+      top: 11,
+      backgroundColor: '#FFF',
+      paddingHorizontal: 10
     }
-});
-
-export default styles;
+  });
+  
+  export default styles;
